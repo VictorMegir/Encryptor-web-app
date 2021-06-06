c64359aac9b4f686e76fa821b1fc01154d37a086
VicMeg <victormegir@gmail.com>
2021-06-06 16:25:24 +0300

    Deploying 8 - changed layout

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+
+// Create Server
+const server = express();
+
+// Using JSON middleware for body parsing
+server.use(express.json());
+
+// Serve static assets if in production
+if(process.env.NODE_ENV == 'production') 
+{
+    server.use(express.static('encryptor/build'));
+    server.use(favicon(path.join(__dirname, 'encryptor', 'build', 'icons', 'favicon.ico')));
+
+    server.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'encryptor', 'build', 'index.html'))
+    })
+}
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`Server listening on port ${port}`));
+
+// Using Router middleware
+const router = require('./encryptor-server/middleware/router');
+server.use('/', router);
