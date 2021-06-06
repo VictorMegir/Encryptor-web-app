1a285cf3d3d7f5eafa5dce6e5270e3d7f5544f03
VicMeg <victormegir@gmail.com>
2021-06-06 14:35:30 +0300

    Deploying

--- encryptor/tailwind.config.js
+++ encryptor/tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  purge: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html'
+  ],
+  darkMode: false,
   theme: {
     extend: {},
   },
