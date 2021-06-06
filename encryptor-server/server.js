7f12f4c6dc5adf2ef536d95edf40f8065d148972
VicMeg <victormegir@gmail.com>
2021-06-06 14:05:55 +0300

    Deploying

--- encryptor-server/server.js
+++ encryptor-server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 // Create Server
 const server = express();
@@ -6,6 +7,14 @@ const server = express();
 // Using JSON middleware for body parsing
 server.use(express.json());
 
+// Serve static assets if in production
+if(process.env.NODE_ENV == 'production') {
+    // Set static folder
+    server.use(express.static('encryptor/build'));
+    server.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'encryptor', 'build', 'index.html'))
+    })
+}
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`Server listening on port ${port}`));
 
